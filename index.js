febc6dcd7c9e9b4da4c99d8ae178b960dd6f76b0
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-19 23:40:29 +0300

    phase-1-array-map-method-lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const tutorials = [
-  'what does the this keyword mean?',
+  'What Does The This Keyword Mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
@@ -9,8 +9,13 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-];
+]
 
-const titleCased = () => {
-  return tutorials
+function titleCase() {
+   return tutorials, map(function (tutorials) {
+     return tutorials.split(' ').map(function(word){
+      return word.charAt(0).toUppercase() + word.slice(1)
+   }).join(' ')
+  })
 }
+  console.log(titleCased())
